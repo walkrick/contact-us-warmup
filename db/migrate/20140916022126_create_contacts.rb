ff468d746f2d56f6649534082755fe43c5c33b42
Bobby Wilson <bobbywilson0@gmail.com>
2014-09-15 21:40:10 -0600

    ...

--- db/migrate/20140916022126_create_contacts.rb
+++ db/migrate/20140916022126_create_contacts.rb
@@ -1,7 +1,7 @@
 class CreateContacts < ActiveRecord::Migration
   def change
     create_table :contacts do |t|
-      #ContctUs.load_data
+      ContctUs.load_data
       t.integer :name
       t.string :email
       t.text :notes
